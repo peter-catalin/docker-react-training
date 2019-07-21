4a0fceb90659153b5ac2200d99db003c6f193350
Peter Catalin <peter.catalin@lendismart.com>
2019-07-21 14:31:33 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -10,13 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Hello, Peter. This code runs in a Nginx image!
         </a>
       </header>
     </div>
